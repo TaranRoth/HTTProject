2cf21ee98d19e46339dfa873a4046a1422eb8821
Taran Roth <taranr5080@gmail.com>
2023-02-01 21:58:17 -0500

    Final commit

--- app.js
+++ app.js
@@ -1,58 +1,124 @@
+// Taran Roth, Periods 7-8 Even, 2/1/23
+
 const express = require('express');
 const app = express();
 app.use(express.json());
 const port = 3000;
-const courses = [
-    {id: 1, name:'Web Development'},
-    {id:2, name: 'IT'},
-    {id:3, name: 'Cybersecurity'},
+const songs = [
+    {id: 1, name:'Jolene', genre: 'pop', month: 'February', year: 1974},
+    {id: 2, name: 'Rondo Alla Turca', genre: 'classical', month: 'August', year: 1784},
+    {id: 3, name: 'Bohemian Rhapsody', genre: 'rock', month: 'October', year: 1975},
 ];
+const genres = ['pop', 'hip-hop', 'rap', 'classical', 'rock', 'jazz', 'blues', 'electronic']
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 app.get('/', (req, res) => {
-    res.send('Hello world!');
+    res.send('Welcome to my music app');
+});
+
+app.get('/api/genres', (req, res) => {
+    res.send('Genres: ' + genres.join(', '));
 })
 
-app.get('/api/courses', (req, res) => {
-    res.send(courses);
+app.get('/api/songs', (req, res) => {
+    let songsStr = ''
+    for (let i = 1; i <= songs.length; i++) {
+        let currentSong = songs.find(s => s.id == i);
+        if (currentSong) songsStr += `${currentSong.id}. ${currentSong.name}, Genre: ${currentSong.genre}\n`;
+    }
+    res.send(songsStr);
 });
 
-app.get('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id == parseInt(req.params.id));
-    if (!course) {
-        res.status(404).send('The course with the given ID was not found.');
+app.get('/api/songs/date/:month/:year', (req, res) => {
+    let filteredSongs = songs.filter(s => s.month == months[parseInt(req.params.month) - 1] && s.year == parseInt(req.params.year));
+    if (filteredSongs.length == 0) {
+        res.status(404).send('The song with the given month and year was not found.');
         return;
     }
-    res.send(course);
+    let songsStr = '';
+    for (let song of filteredSongs) {
+        songsStr += `${song.id}. ${song.name}, Genre: ${song.genre}\n`
+    }
+    res.send(songsStr);
 })
 
-app.post('/api/courses', (req, res) => {
-    if (req.body.name.length < 4) {
-        res.status(422).send('The course name must be at least 4 characters.');
+app.get('/api/songs/month/:month', (req, res) => {
+    let filteredSongs = songs.filter(s => s.month == months[parseInt(req.params.month) - 1]);
+    if (filteredSongs.length == 0) {
+        res.status(404).send('The song with the given month was not found.');
         return;
     }
-    const course = {id: courses.length + 1, name: req.body.name};
-    courses.push(course);
-    res.send(course);
+    let songsStr = '';
+    for (let song of filteredSongs) {
+        songsStr += `${song.id}. ${song.name}, Genre: ${song.genre}\n`
+    }
+    res.send(songsStr);
+})
+
+app.get('/api/songs/year/:year', (req, res) => {
+    let filteredSongs = songs.filter(s =>  s.year == parseInt(req.params.year));
+    if (filteredSongs.length == 0) {
+        res.status(404).send('The song with the given year was not found.');
+        return;
+    }
+    let songsStr = '';
+    for (let song of filteredSongs) {
+        songsStr += `${song.id}. ${song.name}, Genre: ${song.genre}\n`
+    }
+    res.send(songsStr);
 });
 
-app.put('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id == parseInt(req.params.id));
-    if (!course) {
-        res.status(404).send('The course with the given ID was not found.');
+app.get('/api/songs/:id', (req, res) => {
+    const song = songs.find(s => s.id == parseInt(req.params.id));
+    if (!song) {
+        res.status(404).send('The song with the given ID was not found.');
         return;
     }
-    course.name = req.body.name;
-    res.send(course);
+    res.send(song);
 });
 
-app.delete('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id == parseInt(req.params.id));
-    if (!course) {
-        res.status(404).send('The course with the given ID was not found.');
+app.post('/api/genres/:genre', (req, res) => {
+    if (genres.indexOf(req.params.genre) >= 0) {
+        res.status(400).send('The given genre already exists.');
+        return;
+    } else if (req.params.genre.length < 3) {
+        res.status(400).send('The given genre must be a minimum of 3 characters.');
+        return;
+    } else if (req.params.genre.length > 10) {
+        res.status(400).send('The given genre must be 10 characters or less.');
         return;
     }
-    courses.splice(courses.indexOf(course), 1);
-    res.send(course);
+    genres.push(req.params.genre);
+    res.send(`The genre was added successfully.\nCurrently available genres: ${genres.join(', ')}`);
+});
+
+
+
+app.put('/api/genres/:originalGenre/:newGenre', (req, res) => {
+    const genreIndex = genres.indexOf(req.params.originalGenre);
+    if (genreIndex < 0) {
+        res.status(404).send('That genre does not exist.');
+        return;
+    }
+    if (req.params.newGenre.length < 3) {
+        res.status(400).send('The genre must have a minimum of 3 characters.');
+        return;
+    } else if (req.params.newGenre.length >10) {
+        res.status(400).send('The genre can have a maximum of 10 characters.');
+        return;
+    }
+    genres[genreIndex] = req.params.newGenre;
+    res.send(`${req.params.originalGenre} successfully changed to ${req.params.newGenre}`);
+});
+
+app.delete('/api/songs/:id', (req, res) => {
+    const song = songs.find(s => s.id == parseInt(req.params.id));
+    if (!song) {
+        res.status(404).send('The song with the given ID was not found.');
+        return;
+    }
+    songs.splice(songs.indexOf(song), 1);
+    res.send(`Song with ID ${req.params.id} was successfully removed.`);
 });
 
 app.listen(port, () => {
